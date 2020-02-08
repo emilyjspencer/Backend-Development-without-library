f0677ddf2b2bd495122fe180cb95243c0968c1f8
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-08 20:31:32 +0000

    Sets port for server to listen to

--- server.js
+++ server.js
@@ -1,2 +1,6 @@
 var http = require('http');
 var server = http.createServer();
+
+server.listen(3000, function () {
+  console.log("Server is listening on port 3000.  Ready to accept requests!");
+});
