5201d8a6e1a7e150dff7cada9bcf02e0659136a4
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-02-08 20:35:40 +0000

    Adds handler function

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 var http = require('http');
-var server = http.createServer();
+var server = http.createServer(handler);
+
+var message = "The hills are alive with the sound of music"
+function handler (request, response) {
+
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(message); //response body
+  response.end(); // finish response
+}
+
 
 server.listen(3000, function () {
   console.log("Server is listening on port 3000.  Ready to accept requests!");
